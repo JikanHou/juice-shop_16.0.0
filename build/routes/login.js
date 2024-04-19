b0c4d38a06ba8e14acc920504f56ca43e4626bed
JikanHou <62175579+JikanHou@users.noreply.github.com>
2024-04-19 18:25:47 -0400

    fix it Login Admin

--- build/routes/login.js
+++ build/routes/login.js
@@ -54,9 +54,13 @@ module.exports = function login() {
         });
     }
     return (req, res, next) => {
+
         verifyPreLoginChallenges(req); // vuln-code-snippet hide-line
-        models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: user_1.UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-            .then((authenticatedUser) => {
+        // Fix it Login Admin
+        // models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: user_1.UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+        models.sequelize.query(`SELECT * FROM Users WHERE email = $1 AND password = $2 AND deletedAt IS NULL`,
+	      { bind: [ req.body.email, security.hash(req.body.password) ], model: models.User, plain: true })
+        .then((authenticatedUser) => {
             const user = utils.queryResultToJson(authenticatedUser);
             if (user.data?.id && user.data.totpSecret !== '') {
                 res.status(401).json({
